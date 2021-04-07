9214568d8a0403dba89cbed7edad54e820ab2160
Ignacio <ignaciosanchezvilar@gmail>
2021-04-06 23:39:12 -0300

    clase 9

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,18 +1,33 @@
   
-import React from 'react'
-
+import React,{useState} from 'react'
+import {ItemCount} from '../ItemCount'
 import "./ItemDetail.css";
-
+import {Link} from 'react-router-dom';
 
 export default function ItemDetail({ item }) {
- 
-    console.log(process.env.PUBLIC_URL + item?.imagen)
+    const [count, setCount] = useState(0)
+
+    const addHandler = (contador)=>{
+        console.log('se agrego un item', contador)
+        setCount(contador)
+    }
+    
 
     return <>
+
+            {count}
             <img className="photo" src={process.env.PUBLIC_URL + item?.imagen} alt=""/>
             <h2>{item?.nombre}</h2>
             <p>{item?.descripcion}</p>
             <div>{item?.precio}</div>
+            { count == 0 ?
+                    <ItemCount stock="6" initial="2" onAdd={addHandler} />
+                        :
+                        <Link to='/cart'>
+                            <button>Terminar mi compra</button>
+                        </Link> 
+
+            }
   </>;
    
   }
