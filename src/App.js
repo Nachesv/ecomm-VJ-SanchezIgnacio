2aa4de04596394c7c5d9a0b12fc3d30b99120614
Ignacio <ignaciosanchezvilar@gmail>
2021-04-08 22:38:06 -0300

    Context

--- src/App.js
+++ src/App.js
@@ -3,28 +3,32 @@ import './App.css';
 import {NavBar} from './components/NavBar/NavBar.js';
 import {ItemListContainer} from './components/ItemListContainer/ItemListContainer.js';
 import ItemDetailContainer from "./components/ItemDetailContainer/ItemDetailContainer.js";
+import CartProvider from "./context/cartContext.js";
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {useState} from 'react';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <NavBar /> 
-        <Switch>
-          <Route path="/" exact component={ItemListContainer}></Route>
-					
-          <Route path="/category/:categoryId">
-						<ItemListContainer greeting="Bienvenidos"/>
-					</Route>
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <NavBar /> 
+          <Switch>
+            <Route path="/" exact component={ItemListContainer}></Route>
+            
+            <Route path="/category/:categoryId">
+              <ItemListContainer greeting="Bienvenidos"/>
+            </Route>
 
-					<Route path="/item/:itemId">
-						<ItemDetailContainer />
-					</Route>
+            <Route path="/item/:itemId">
+              <ItemDetailContainer />
+            </Route>
 
-          
-        </Switch>
-      </div>
-    </Router>
+            
+          </Switch>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
